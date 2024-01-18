72b81480f16a275cd57d277f9df752f700c57a7a
Маметьев Сергей <279013766@TOP.ACADEMY>
2024-01-18 20:47:49 +0300

    users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    setLoading(true);
+    setError('')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .finally(() => setLoading(false))
+      .then(res => res.json())
+      .then(users => setUsers(users))
+      .catch(error=> setError(error.toString()))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loading ? <h1>Loading...</h1> :
+        error ? <p>Error:{ error }</p>:
+        users.map(user => (<>
+          <h5>{ user.name }</h5>
+          <p>{ user.email }</p>
+        </>))
+      }
     </div>
   );
 }
